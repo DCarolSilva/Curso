3ab75e0dfebaeb53d53c952da0870e0f7be14a47
Daniela Silva Laguna <sildanny2000@gmail.com>
2022-11-03 16:52:59 -0700

    avance depot 03_11_22

--- depot/app/controllers/concerns/current_cart.rb
+++ depot/app/controllers/concerns/current_cart.rb
@@ -6,8 +6,8 @@ module CurrentCart
             @cart = Cart.find(session[:cart_id]) #intenta obtener el carrito actual
         
             
-        rescue ActiveRecord::RecordNotFaound
-            @cart = Cart.created
+        rescue ActiveRecord::RecordNotFound
+            @cart = Cart.create
             session[:cart_id] = @cart.id
         end
 
--- depot/app/controllers/store_controller.rb
+++ depot/app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
+  #se salta la validacion de autentificacion
+  skip_before_action :authenticate_user!
   def index
     @products = Product.order(:title)
   end
